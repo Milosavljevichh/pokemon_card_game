95e41ba74e8ea204f9cd006d55cc37c33c5fbfa5
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2024-08-25 01:32:33 +0200

    lightly styled

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -3,9 +3,9 @@ import Scoreboard from "./scoreboard";
 function Header({currentScore, bestScore}) {
     return(
         <div id="header">
-            <div>
-                <h1>Memorize Cats</h1>
-                <h4>Click on a picture, without clicking the same image twice</h4>
+            <div id="title">
+                <h1>Memorize Pokemon</h1>
+                <h4>Click on a picture, without clicking the same picture twice</h4>
             </div>
             <Scoreboard currentScore={currentScore} bestScore={bestScore} />
         </div>
--- app/src/components/Scoreboard.jsx
+++ app/src/components/Scoreboard.jsx
@@ -1,7 +1,7 @@
 function Scoreboard({currentScore, bestScore}) {
     return (
-        <div>
-            <h2>score: {currentScore}</h2>
+        <div  id="scoreboard">
+            <h2>Score: {currentScore}</h2>
             <h2>Best-score: {bestScore}</h2>
         </div>
     )
